37a3dd193e1bc904cbe175bcffc36d772478a36a
qsangdev <quangsang02018@gmail.com>
2022-12-19 16:34:04 +0700

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.png";
+import "./app.css";
+import axios from "axios";
+import ReactCanvasConfetti from "./confetti/confetti";
 
 function App() {
+  const [getLink, setGetLink] = useState("");
+  const [stateOnChangeGetLink, setStateOnChangeGetLink] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [stateButton, setStateButton] = useState("");
+  const [showResult, setShowResult] = useState(false);
+  const onClickCallApi = async () => {
+    setLoading(true);
+    const response = await axios.get(
+      `https://api.shrtco.de/v2/shorten?url=${stateOnChangeGetLink}`
+    );
+    const { short_link, short_link2, short_link3 } = response.data.result;
+    console.log(stateButton === "short_link2");
+    if (stateButton === "short_link1") {
+      setGetLink(short_link);
+    } else if (stateButton === "short_link2") {
+      setGetLink(short_link2);
+    } else if (stateButton === "short_link3") {
+      setGetLink(short_link3);
+    } else if (stateButton === "") {
+      setGetLink(short_link);
+    }
+    setLoading(false);
+    setShowResult(true);
+  };
+  const getLinkOnChange = (event) => {
+    setStateOnChangeGetLink(event.target.value);
+  };
+
+  const getButtonClick = (e) => {
+    setStateButton(e);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="app-container">
+      <img src={logo} alt="logo" />
+      <h1>
+        The <b>privacy-friendly</b> URL Shortener
+      </h1>
+      <div className="container">
+        <div className="input-box">
+          <h2>Link Shortener</h2>
+          <span>Enter a Link: </span>
+          <input
+            type="text"
+            placeholder="example.com"
+            onChange={getLinkOnChange}
+          />
+          <button onClick={onClickCallApi}>
+            <i
+              id={loading ? "display-none" : ""}
+              className="fa-solid fa-arrow-right"
+            ></i>
+            {loading && <i className="fa-solid fa-spinner fa-spin-pulse"></i>}
+          </button>
+        </div>
+        <div className="link-box">
+          <span>Short domain: </span>
+          <button onClick={() => getButtonClick("short_link1")}>
+            shrtco.de
+          </button>
+          <button onClick={() => getButtonClick("short_link2")}>9qr.de</button>
+          <button onClick={() => getButtonClick("short_link3")}>
+            shiny.link
+          </button>
+        </div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          With this free Link Shortener you can make Links shorter and easier to
+          remember. Just enter a Link into the form and click on the above
+          Button to generate a short Link. When visiting the short-Link, the
+          short-Link will immediately redirect you to the long Link.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <p>By using shrtcode you agree to our Terms of Service.</p>
+      {showResult && (
+        <div className="result">
+          <label htmlFor="">Link generated!</label>
+          <a href={`http://${getLink}`} target="_blank" rel="noreferrer">
+            {getLink}
+          </a>
+        </div>
+      )}
+      {showResult && <ReactCanvasConfetti />}
+      
+      <div className="footer">© 2022 shrtcode by QS </div>
     </div>
   );
 }
